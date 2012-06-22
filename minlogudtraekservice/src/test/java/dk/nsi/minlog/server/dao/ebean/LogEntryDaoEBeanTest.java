f8a92bb1d36fc4cd6fb7855797b63d217f839d34
krpiatkowski <kr.piatkowski@gmail.com>
2012-06-22 13:34:02 +0200

    Fixed a todo about better assertions in test

--- minlogudtraekservice/src/test/java/dk/nsi/minlog/server/dao/ebean/LogEntryDaoEBeanTest.java
+++ minlogudtraekservice/src/test/java/dk/nsi/minlog/server/dao/ebean/LogEntryDaoEBeanTest.java
@@ -1,8 +1,10 @@
 package dk.nsi.minlog.server.dao.ebean;
 
+import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
@@ -17,6 +19,8 @@ import com.avaje.ebean.EbeanServer;
 import dk.nsi.minlog.domain.LogEntry;
 import dk.nsi.minlog.server.dao.LogEntryDao;
 
+import static org.junit.Assert.*;
+
 @RunWith(MockitoJUnitRunner.class)
 public class LogEntryDaoEBeanTest {
 
@@ -26,24 +30,39 @@ public class LogEntryDaoEBeanTest {
 	@Mock(answer=Answers.RETURNS_DEEP_STUBS)
 	EbeanServer ebeanServer;
 		
+	public List<LogEntry> createResult(){
+		List<LogEntry> entries = new ArrayList<LogEntry>();
+		entries.add(new LogEntry());
+		entries.add(new LogEntry());
+		entries.add(new LogEntry());		
+		return entries;
+	}
 	
-	//TODO assert on returns from ebean
 	@Test
-	public void findByCpr() throws Exception {
-		when(ebeanServer.find(LogEntry.class).where().eq("cprNrBorger", "1234").findList()).thenReturn(new ArrayList<LogEntry>());
-		logEntryDao.findLogEntriesByCPRAndDates("1234", null, null);
+	public void findByCpr() throws Exception {			
+		List<LogEntry> entries = createResult();
+		when(ebeanServer.find(LogEntry.class).where().eq("cprNrBorger", "1234").findList()).thenReturn(entries);
+		List<LogEntry> result = logEntryDao.findLogEntriesByCPRAndDates("1234", null, null);
+		
+		assertEquals(entries, result);		
 	}
 
 	@Test
 	public void findByCprAndFrom() throws Exception {
-		when(ebeanServer.find(LogEntry.class).where().eq("cprNrBorger", "1234").ge(eq("tidspunkt"), any()).findList()).thenReturn(new ArrayList<LogEntry>());		
-		logEntryDao.findLogEntriesByCPRAndDates("1234", DateTime.now(), null);
+		List<LogEntry> entries = createResult();
+		when(ebeanServer.find(LogEntry.class).where().eq("cprNrBorger", "1234").ge(eq("tidspunkt"), any()).findList()).thenReturn(entries);		
+		List<LogEntry> result = logEntryDao.findLogEntriesByCPRAndDates("1234", DateTime.now(), null);
+		
+		assertEquals(entries, result);		
 	}
 
 	@Test
 	public void findByCprAndTo() throws Exception {
-		when(ebeanServer.find(LogEntry.class).where().eq("cprNrBorger", "1234").le(eq("tidspunkt"), any()).findList()).thenReturn(new ArrayList<LogEntry>());		
-		logEntryDao.findLogEntriesByCPRAndDates("1234", null, DateTime.now());
+		List<LogEntry> entries = createResult();
+		when(ebeanServer.find(LogEntry.class).where().eq("cprNrBorger", "1234").le(eq("tidspunkt"), any()).findList()).thenReturn(entries);		
+		List<LogEntry> result = logEntryDao.findLogEntriesByCPRAndDates("1234", null, DateTime.now());
+		
+		assertEquals(entries, result);		
 	}
 
 }
